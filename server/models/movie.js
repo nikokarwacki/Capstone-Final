dab74ceeb4ac0f2e21fac31c0672973e864a3441
Niko Karwacki <nikokarwacki@gmail.com>
2026-03-29 19:19:31 -0400

    completed MVP full-stack movie save workflow

--- server/models/movie.js
+++ server/models/movie.js
@@ -2,10 +2,19 @@ module.exports = (sequelize, DataTypes) => {
   const Movie = sequelize.define('Movie', {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     genre: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+    },
+    posterPath: {
+      type: DataTypes.STRING,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+    },
+    overview: {
+      type: DataTypes.TEXT,
     }
   });
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./models');
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -18,4 +20,6 @@ db.sequelize.sync().then(() => {
   app.listen(3000, () => {
     console.log('Server running on port 3000');
   });
+}).catch((error) => {
+  console.error('Database error:', error);
 });
